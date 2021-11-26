9e95a2f8770ba9cc96c9ae6aa98ff18a4740535d
gustavo henrique <gustavohenrique3699@gmail.com>
2021-11-26 18:38:20 -0300

    criaçao do programa 3

--- fundamentos/bloco4-Introdução-à-JavaScript-e-Lógica-de-Programação/index.js
+++ fundamentos/bloco4-Introdução-à-JavaScript-e-Lógica-de-Programação/index.js
@@ -20,15 +20,31 @@
 
 // 2
 
-const a2 = 10;
-const b2 = 15;
+// const a2 = 10;
+// const b2 = 15;
 
-if (a2 > b2) {
-    console.log (a2);
+// if (a2 > b2) {
+//     console.log (a2);
+// }
+// else if (b2 > a2) {
+//     console.log(b2);
+// }
+// else {
+//     console.log("são iguais");
+// }
+
+// 3
+
+const a3 = 100;
+const b3 = 50;
+const c3 = 25;
+
+if (a3 > b3 && a3 > c3) {
+    console.log("a3 é o maior número")
 }
-else if (b2 > a2) {
-    console.log(b2);
+else if (b3 > a3 && b3 > c3) {
+    console.log("b3 é o maior número")
 }
 else {
-    console.log("são iguais");
+    console.log("c3 é o maior número")
 }
