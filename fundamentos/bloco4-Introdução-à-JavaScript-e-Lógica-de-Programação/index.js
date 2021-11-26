0f852d1f1e8f4ccb2b6ccc911deea7d6b8106825
gustavo henrique <gustavohenrique3699@gmail.com>
2021-11-26 18:34:28 -0300

    criaçao do programa 2

--- fundamentos/bloco4-Introdução-à-JavaScript-e-Lógica-de-Programação/index.js
+++ fundamentos/bloco4-Introdução-à-JavaScript-e-Lógica-de-Programação/index.js
@@ -1,17 +1,34 @@
-const a = 10;
-const b = 15;
+//1
 
-const adicao = a + b;
-console.log(adicao);
+// const a = 10;
+// const b = 15;
 
-const subtracao = a - b;
-console.log(subtracao)
+// const adicao = a + b;
+// console.log(adicao);
 
-const multiplicacao = a * b;
-console.log(multiplicacao)
+// const subtracao = a - b;
+// console.log(subtracao)
 
-const divisao = a / b;
-console.log(divisao)
+// const multiplicacao = a * b;
+// console.log(multiplicacao)
 
-const modulo = a % b;
-console.log(modulo)
+// const divisao = a / b;
+// console.log(divisao)
+
+// const modulo = a % b;
+// console.log(modulo)
+
+// 2
+
+const a2 = 10;
+const b2 = 15;
+
+if (a2 > b2) {
+    console.log (a2);
+}
+else if (b2 > a2) {
+    console.log(b2);
+}
+else {
+    console.log("são iguais");
+}
