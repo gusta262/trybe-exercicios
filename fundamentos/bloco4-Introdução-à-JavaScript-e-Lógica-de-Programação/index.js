3e9a643c7b55dc7db9cc4196021c5beaf795bdf8
gustavo henrique <gustavohenrique3699@gmail.com>
2021-11-26 18:21:46 -0300

    adiciona operadores

---
+++ fundamentos/bloco4-Introdução-à-JavaScript-e-Lógica-de-Programação/index.js
@@ -0,0 +1,17 @@
+const a = 10;
+const b = 15;
+
+const adicao = a + b;
+console.log(adicao);
+
+const subtracao = a - b;
+console.log(subtracao)
+
+const multiplicacao = a * b;
+console.log(multiplicacao)
+
+const divisao = a / b;
+console.log(divisao)
+
+const modulo = a % b;
+console.log(modulo)
