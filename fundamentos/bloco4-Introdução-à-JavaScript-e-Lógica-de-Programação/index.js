12ab165dadd7cf81faca30d65e42c58dcd516c18
gustavo henrique <gustavohenrique3699@gmail.com>
2021-11-29 16:06:56 -0300

    resoluçao do exercicio 2

--- fundamentos/bloco4-Introdução-à-JavaScript-e-Lógica-de-Programação/index.js
+++ fundamentos/bloco4-Introdução-à-JavaScript-e-Lógica-de-Programação/index.js
@@ -1,6 +1,12 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 // ex 1 
 
+// for (numbers of numbers) {
+//     console.log(numbers);
+// }
+
+//ex 2
+
 for (numbers of numbers) {
-    console.log(numbers);
+    console.log(numbers += 1);
 }
