137b461e48e1c3eb37bb92e909d38889fe064cef
gustavo henrique <gustavohenrique3699@gmail.com>
2021-11-29 18:04:39 -0300

    resoluçao do exercicio 3

--- fundamentos/bloco4-Introdução-à-JavaScript-e-Lógica-de-Programação/index.js
+++ fundamentos/bloco4-Introdução-à-JavaScript-e-Lógica-de-Programação/index.js
@@ -7,6 +7,16 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 //ex 2
 
-for (numbers of numbers) {
-    console.log(numbers += 1);
+// for (numbers of numbers) {
+//     console.log(numbers += 1);
+// }
+
+//ex 3
+let conta = 0;
+
+for (let index = 0; index < numbers.length; index += 1) {
+    conta += numbers[index]
 }
+
+conta = conta / numbers.length
+console.log(conta)
