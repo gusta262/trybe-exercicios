8477e2d009f4e3e43b11cca74f8db4261921658a
gustavo henrique <gustavohenrique3699@gmail.com>
2021-11-29 18:14:34 -0300

    resoluçao do exercicio 5

--- fundamentos/bloco4-Introdução-à-JavaScript-e-Lógica-de-Programação/index.js
+++ fundamentos/bloco4-Introdução-à-JavaScript-e-Lógica-de-Programação/index.js
@@ -23,17 +23,29 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 // console.log(conta)
 
 //ex 4
-let conta = 0;
+
+// let conta = 0;
+
+// for (let index = 0; index < numbers.length; index += 1) {
+//     conta += numbers[index];
+// }
+    
+// conta = conta / numbers.length;
+
+// if (conta > 20) {
+//     console.log('valor maior que 20');
+// }
+// else {
+//     console.log('valor menor ou igual a 20');
+// }
+
+//ex 5 
+let maioral = 0;
 
 for (let index = 0; index < numbers.length; index += 1) {
-    conta += numbers[index];
+    if (numbers[index] > maioral) {
+        maioral = numbers[index];
+    }
 }
-    
-conta = conta / numbers.length;
 
-if (conta > 20) {
-    console.log('valor maior que 20');
-}
-else {
-    console.log('valor menor ou igual a 20');
-}
+console.log(maioral);
