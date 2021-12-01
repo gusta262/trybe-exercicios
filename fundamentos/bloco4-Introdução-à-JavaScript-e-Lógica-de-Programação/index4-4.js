f575ce446f53634ceedad3fb0a17f95fe8455a68
gustavo henrique <gustavohenrique3699@gmail.com>
2021-12-01 18:25:00 -0300

    exercicio 5

--- fundamentos/bloco4-Introdução-à-JavaScript-e-Lógica-de-Programação/index4-4.js
+++ fundamentos/bloco4-Introdução-à-JavaScript-e-Lógica-de-Programação/index4-4.js
@@ -4,6 +4,24 @@ let info = {
     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
     recorrente: 'Sim',
   };
-  for (let index in info) {
-      console.log(info[index]);
+  
+let info2 = {
+    personagem: 'Tio Patinhas',
+    origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+    nota: 'O último MacPatinhas',
+    recorrente: 'Sim',
+  };
+
+for (let index1 in info) {
+  for (let index in info2) {
+    if (info[index1] === info2[index]) {
+      console.log('ambos recorrentes');
+    }
+    else if (index1 === index) {
+      console.log(info[index1], "e", info2[index])
+    }
   }
+}
+
+
+
