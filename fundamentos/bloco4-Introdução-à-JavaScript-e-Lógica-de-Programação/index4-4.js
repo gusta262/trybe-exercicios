bafd2ce9c8d6e5c17d8b5241bc0c7d369b57eff4
gustavo henrique <gustavohenrique3699@gmail.com>
2021-12-01 17:59:09 -0300

    exercicio 3

--- fundamentos/bloco4-Introdução-à-JavaScript-e-Lógica-de-Programação/index4-4.js
+++ fundamentos/bloco4-Introdução-à-JavaScript-e-Lógica-de-Programação/index4-4.js
@@ -4,4 +4,6 @@ let info = {
     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
     recorrente: 'Sim',
   };
-  console.log(info);
+  for (let index in info) {
+      console.log(index);
+  }
