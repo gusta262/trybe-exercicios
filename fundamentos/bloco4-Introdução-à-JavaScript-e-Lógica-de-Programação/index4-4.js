848bf9225cb408b67c1f57a1cf6faa6316d9e23b
gustavo henrique <gustavohenrique3699@gmail.com>
2021-12-01 18:40:19 -0300

    exercicio 6

--- fundamentos/bloco4-Introdução-à-JavaScript-e-Lógica-de-Programação/index4-4.js
+++ fundamentos/bloco4-Introdução-à-JavaScript-e-Lógica-de-Programação/index4-4.js
@@ -1,27 +1,14 @@
-let info = {
-    personagem: 'Margarida',
-    origem: 'Pato Donald',
-    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
-    recorrente: 'Sim',
-  };
-  
-let info2 = {
-    personagem: 'Tio Patinhas',
-    origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
-    nota: 'O último MacPatinhas',
-    recorrente: 'Sim',
-  };
+let leitor = {
+  nome: 'Julia',
+  sobrenome: 'Pessoa',
+  idade: 21,
+  livrosFavoritos: [
+    {
+      titulo: 'O Pior Dia de Todos',
+      autor: 'Daniela Kopsch',
+      editora: 'Tordesilhas',
+    },
+  ],
+};
 
-for (let index1 in info) {
-  for (let index in info2) {
-    if (info[index1] === info2[index]) {
-      console.log('ambos recorrentes');
-    }
-    else if (index1 === index) {
-      console.log(info[index1], "e", info2[index])
-    }
-  }
-}
-
-
-
+console.log('o livro favorito de', leitor.nome, leitor.sobrenome, 'se chama', leitor.livrosFavoritos[0].titulo)
