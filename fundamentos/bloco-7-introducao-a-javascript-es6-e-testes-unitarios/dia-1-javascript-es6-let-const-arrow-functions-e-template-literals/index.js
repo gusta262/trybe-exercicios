5c8ec27216d7da8c9fd95389d07cf28879b3a940
gustavo henrique <gustavohenrique3699@gmail.com>
2022-01-12 17:53:33 -0300

    exercicio 2 parte 1

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/index.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/index.js
@@ -1,12 +1,18 @@
-    const testingScope = escopo => {
-    if (escopo === true) {
-      let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
-      ifScope = `${ifScope} ótimo, fui utilizada no escopo!`;
-      console.log(ifScope);
-    } else {
-      const elseScope = 'Não devo ser utilizada fora meu escopo (else)';
-      console.log(elseScope);
-    }
-  }
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
 
-  testingScope(true);
+// Seu código aqui.
+
+const oddsFun = () => {
+    oddsAndEvens[0] = 2
+    oddsAndEvens[1] = 3
+    oddsAndEvens[2] = 4
+    oddsAndEvens[3] = 7
+    oddsAndEvens[4] = 10
+    oddsAndEvens[5] = 13
+
+    return oddsAndEvens
+}
+
+const oddsOrdem = oddsFun();
+
+console.log(oddsOrdem); // será necessário alterar essa linha 😉
