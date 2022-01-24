078ed86ca3d11d290635c0d3cd732ca810a36d81
gustavo henrique <gustavohenrique3699@gmail.com>
2022-01-24 17:58:24 -0300

    exercicio 8, utilizando de default params

---
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercise8.js
@@ -0,0 +1,7 @@
+// escreva greet abaixo
+
+const greet = (name, greet = 'oi') => `${greet} ${name}`
+
+console.log(greet('John')) // 'Hi John'
+console.log(greet('John', 'Good morning')) // 'Good morning John'
+console.log(greet('Isabela', 'Oi')) // 'Oi Isabela'
