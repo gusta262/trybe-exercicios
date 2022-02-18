5d65655b41743bfa7dfb6fcc49013a41aeb8ff80
gustavo henrique <gustavohenrique3699@gmail.com>
2022-02-18 00:25:50 -0300

    finalizando minha pokedexgit add . ORGULHO

--- front-end/bloco-10-introducao-a-react/dia-2-componentes-react/my-pokedex/src/App.js
+++ front-end/bloco-10-introducao-a-react/dia-2-componentes-react/my-pokedex/src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import ArquivoTwo from './components/ArquivoTwo';
+import ListPoks from './components/ListPoks'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>POKEDEX</h1>
+      <ArquivoTwo poke={ ListPoks }/>
     </div>
   );
 }
